752a6ae2d1514e4c31631dcd0604173f43783c8a
jangramukul <github.starring536@passmail.net>
2026-03-22 17:06:38 +0000

    cross ref

--- app/src/main/java/com/wise/code/room/sample/data/local/SongDao.kt
+++ app/src/main/java/com/wise/code/room/sample/data/local/SongDao.kt
@@ -1,4 +1,16 @@
 package com.wise.code.room.sample.data.local
 
-class SongDao {
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+
+@Dao
+interface SongDao {
+    @Transaction
+    @Query("SELECT * FROM songs")
+    suspend fun getSongsWithPlaylist(): SongWithPlaylists
+
+    @Transaction
+    @Query("SELECT * FROM playlists")
+    suspend fun getPlaylistsWithSongs(): PlaylistWithSongs
 }
--- app/src/main/java/com/wise/code/room/sample/data/local/SongEntity.kt
+++ app/src/main/java/com/wise/code/room/sample/data/local/SongEntity.kt
@@ -1,4 +1,55 @@
 package com.wise.code.room.sample.data.local
 
-class SongEntity {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.Junction
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+
+@Entity(tableName = "songs")
+data class SongEntity(
+    @PrimaryKey val id: Int,
+)
+
+@Entity(tableName = "playlists")
+data class PlaylistEntity(
+    @PrimaryKey val id: Int,
+)
+
+// Junction Table / Cross Ref / Many-to-many relationships
+@Entity(primaryKeys = ["song_id", "playlist_id"])
+data class PlaylistAndSongsCrossRef(
+    @ColumnInfo("song_id") val songId: Int,
+    @ColumnInfo("playlist_id") val playlistId: Int,
+)
+
+// Left Join - Many-to-many: Given a song, fetch all playlists it contains
+data class SongWithPlaylists(
+    @Embedded val song: SongEntity,
+    @Relation(
+        parentColumn = "id", // Relation - SongEntity
+        entityColumn = "id", // Relation - PlaylistEntity
+        associateBy = Junction(
+            value = PlaylistAndSongsCrossRef::class,
+            parentColumn = "song_id", // Column in CrossRef matching SongEntity.id
+            entityColumn = "playlist_id", // Column in CrossRef matching PlaylistEntity.id
+        )
+    )
+    val playlists: List<PlaylistEntity>
+)
+
+// Left join - Many-to-many: Given a Playlist, fetch all Songs it contains
+data class PlaylistWithSongs(
+    @Embedded val playlist: PlaylistEntity,
+    @Relation(
+        parentColumn = "id", // PlaylistEntity
+        entityColumn = "id", // SongEntity
+        associateBy = Junction(
+            value = PlaylistAndSongsCrossRef::class,
+            parentColumn = "playlist_id", // Column in CrossRef matching PlaylistEntity.id
+            entityColumn = "song_id" // Column in CrossRef matching SongEntity.id
+        )
+    )
+    val songs: List<SongEntity>
+)
